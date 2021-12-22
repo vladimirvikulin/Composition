73daa0a149137746ba8b6d195ed0b49fb036b0d6
vladimirvikulin <v.vikulin.im11@kpi.ua>
2021-12-22 13:50:53 +0200

    Lab 6 Composition

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,27 @@
 'use strict';
 
-const pipe = (...fns) => x => null;
+const pipe = (...fns) => x => {
+  for (const value of fns) {
+    if (typeof(value) !== 'function') throw new Error('Expected function');
+  }
+  return fns.reduce((value, fn) => fn(value), x);
+};
+
+const inc = x => ++x;
+const twice = x => x * 2;
+const cube = x => x ** 3;
+
+const f = pipe(inc, twice, cube);
+const x = f(5);
+console.log(x); // 1728
+
+// const f = pipe(inc, inc);
+// const x = f(7);
+// console.log(x);
+
+const fn = pipe(inc, 7, cube);
+
+fn(); // error
+
 
 module.exports = { pipe };
--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,37 @@
 'use strict';
 
-const compose = (...fns) => x => null;
+const compose = (...fns) => {
+  const handlers = [];
+  const composed = x => {
+    if (fns.length === 0) return x;
+    const last = fns.length - 1;
+    let res = x;
+    try {
+      for (let i = last; i >= 0; i--) {
+        res = fns[i](res);
+      }
+      return res;
+    } catch (error) {
+      for (const handler of handlers) {
+        handler(error);
+      }
+    }
+  };
+  composed.on = (name, handler) => {
+    if (name === 'error') handlers.push(handler);
+  };
+  return composed;
+};
+
+const error = () => {
+  throw new Error('Hello, its me');
+};
+const inc = x => ++x;
+const twice = x => x * 2;
+const cube = x => x ** 3;
+
+const f = compose(error, cube, twice, inc);
+const x = f(5);
+console.log(x);//undefined
 
 module.exports = { compose };
